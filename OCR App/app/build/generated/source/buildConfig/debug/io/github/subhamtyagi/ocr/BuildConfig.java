c60a42efe1b6c43ea53b15024d31a89220b7ec62
Vishwa Kumaresh <vishwakumaresh@gmail.com>
2024-06-04 20:20:40 +0530

    Commit

---
+++ OCR App/app/build/generated/source/buildConfig/debug/io/github/subhamtyagi/ocr/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package io.github.subhamtyagi.ocr;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "io.github.subhamtyagi.ocr";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 12;
+  public static final String VERSION_NAME = "4.0";
+}
